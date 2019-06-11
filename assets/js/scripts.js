5ac0bab1fe61d8c45234eb2a2c7deb1974bf0f5d
michael-damon <51724668+michael-damon@users.noreply.github.com>
2019-06-11 23:23:25 +0100

    Initial commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,64 @@
+
+(function ($) {
+
+    "use strict";
+
+	// RADIAL PROGRESS BAR
+	enableRadialProgress();
+	
+	
+	$('a[href="#"]').on('click', function(event){
+		return;
+	});
+	
+	
+})(jQuery);
+
+
+
+function enableRadialProgress(){
+	
+	$(".radial-progress").each(function(){
+		var $this = $(this),
+			progPercent = $this.data('prog-percent');
+			
+		var bar = new ProgressBar.Circle(this, {
+			color: '#aaa',
+			strokeWidth: 3,
+			trailWidth: 1,
+			easing: 'easeInOut',
+			duration: 1400,
+			text: {
+				
+			},
+			from: { color: '#aaa', width: 1 },
+			to: { color: '#007bff', width: 3 },
+			// Set default step function for all animate calls
+			step: function(state, circle) {
+				circle.path.setAttribute('stroke', state.color);
+				circle.path.setAttribute('stroke-width', state.width);
+
+				var value = Math.round(circle.value() * 100);
+				if (value === 0) {
+					circle.setText('');
+				} else {
+					circle.setText(value);
+				}
+
+			}
+		});
+		
+		$(this).waypoint(function(){
+		   bar.animate(progPercent);  
+		},{offset: "90%"})
+		
+	});
+}
+
+
+function isExists(elem){
+	if ($(elem).length > 0) { 
+		return true;
+	}
+	return false;
+}
